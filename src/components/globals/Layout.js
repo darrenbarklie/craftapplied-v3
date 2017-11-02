bfb42fc1739743d2fda6063251bebb85717ce86d
Darren Barklie <darrenbarklie@gmail.com>
2017-11-02 13:31:32 +0000

    Layout fixes

--- src/components/globals/Layout.js
+++ src/components/globals/Layout.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
+import { Grid } from 'react-flexbox-grid'
 import { Header } from './header'
 
 import { Home } from '../home/Home'
@@ -10,15 +11,17 @@ export const Layout = () => {
 
       <Header />
 
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" render={() => <h1>About</h1>} />
-        <Route path="/services" render={() => <h1>Services</h1>} />
-        <Route path="/work" render={() => <h1>Work</h1>} />
-        <Route path="/blog" render={() => <h1>Blog</h1>} />
-        <Route path="/contact" render={() => <h1>Contact</h1>} />
-        <Route render={() => <h1>404</h1>} />
-      </Switch>
+      <Grid fluid className="grid-container stage">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" render={() => <h1>About</h1>} />
+          <Route path="/services" render={() => <h1>Services</h1>} />
+          <Route path="/work" render={() => <h1>Work</h1>} />
+          <Route path="/blog" render={() => <h1>Blog</h1>} />
+          <Route path="/contact" render={() => <h1>Contact</h1>} />
+          <Route render={() => <h1>404</h1>} />
+        </Switch>
+      </Grid>
 
     </div>
   )
